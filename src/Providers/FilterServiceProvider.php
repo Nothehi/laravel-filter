ae6ae3ba385ddcd908ab27a528e591a518984613
Hossein <nodehi7813@gmail.com>
2019-09-15 15:20:38 +0430

    Added service provider file

---
+++ src/Providers/FilterServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace HChamran\LaravelFilter\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FilterServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}

3b0e04ab33407b56f964f69d901061ebb398ea8b
Hossein <nodehi7813@gmail.com>
2019-09-18 11:09:58 +0430

    Added filter query bulder class

---
+++ src/Contracts/QueryBuilder.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace HChamran\LaravelFilter\Contracts;
+
+
+use Illuminate\Http\Request;
+
+abstract class QueryBuilder implements FilterInterface
+{
+    protected $request;
+
+    protected $target;
+
+    protected $sort;
+
+
+
+    public function __construct()
+    {
+        $this->fields = $this->fields();
+
+        $this->request = Request::capture();
+
+        list($this->target, $this->sort) = $this->requestParser($this->getRequests());
+    }
+
+
+    /**
+     * Get all requests
+     *
+     * @return array
+     */
+    protected function getRequests()
+    {
+        return $this->request->all();
+    }
+
+    /**
+     * Parse get url string
+     *
+     * @param $request
+     * @return array|void
+     */
+    private function requestParser($request)
+    {
+        if (empty($request))
+            return;
+
+        $options = explode(',', $query);
+        return $options;
+    }
+
+}

273e434d460ef340f7d28465be56269884646fe4
Hossein <nodehi7813@gmail.com>
2019-09-18 13:45:22 +0430

    Added filter helper function

--- src/Helpers/filter.php
+++ src/Helpers/filter.php
@@ -1,1 +1,17 @@
 <?php
+
+if (! function_exists('filter')) {
+    /**
+     * Get the available container instance.
+     *
+     * @param null $class
+     * @return mixed
+     */
+    function filter($class = null)
+    {
+        $filter_class = 'App\Filters\\' . $class;
+        if (class_exists($filter_class)) {
+            return new $filter_class();
+        }
+    }
+}
